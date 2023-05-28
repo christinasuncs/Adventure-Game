94c9506673a00e7cd01d120aa39778fda2adc421
Rohan Sane <rsane@bayviewglen.ca>
2023-05-28 10:36:11 -0400

    Finished items

--- src/zork/Zork.java
+++ src/zork/Zork.java
@@ -3,8 +3,6 @@ package zork;
 public class Zork {
   // this is a commit test
   public static void main(String[] args) {
-    // Rohan
-    // Vanessa
     Game game = new Game();
     game.play();
   }
